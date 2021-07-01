f6de8ab74afece5fbe5529c37dc6b7772ca16f65
dnjstjr93 <dnjstjr93@gmail.com>
2021-07-01 23:20:30 +0900

    - opened serial

--- lib_remote_gimbal.js
+++ lib_remote_gimbal.js
@@ -673,7 +673,7 @@ function channel_val() {
     // rxbuf += '00';
     // console.log(rxbuf);
     console.log(Buffer.from(rxbuf, 'hex'));
-    // sbusPort.write(Buffer.from(rxbuf, 'hex'))
+    sbusPort.write(Buffer.from(rxbuf, 'hex'))
     sbusData();
 }
 
@@ -719,7 +719,7 @@ function sbusData () {
 }
 
 
-// sbusPortOpening();
+sbusPortOpening();
 
 function sbusPortOpening() {
     if (sbusPort == null) {
